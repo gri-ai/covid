4fea2f368b60226a393d2f254bef0f3dc70953d7
gozcu <emrah.gozcu@gmail.com>
2020-05-19 12:47:03 +0100

    Clean up

--- chatbot/chatbot/rest.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-from django.http import JsonResponse, JsonResponse
-from django.shortcuts import render
-
-
-
-def api(request, phone, message, agent):
-    return JsonResponse({'p': phone, 'm':message})
